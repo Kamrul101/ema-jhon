ee5c9a501e06ea35fb182cb47dab0aca67cfc8bc
Kamrul101 <ridoyhasan87@gmail.com>
2023-03-31 20:14:53 +0600

    update

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,41 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import { addToDb } from '../../utilities/fakedb';
-import Cart from '../Cart/Cart';
-import Product from '../Product/Product';
-import './Shop.css'
-
+import React, { useEffect, useState } from "react";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
+import Product from "../Product/Product";
+import "./Shop.css";
 
 const Shop = () => {
-    const [products , setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
-    useEffect(()=>{
-        fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data))
-    },[])
-    const handlerToClick = (product) =>{
-        const newCart = [...cart, product];
-        setCart(newCart);
-        addToDb(product.id)   
-    }
-    
-    return (
-        <div className='shop-container'>
-            <div className='product-container'>
-                {
-                    products.map(product => <Product key={product.id}
-                    product={product}
-                    handlerToClick={handlerToClick}
-                    ></Product>)
-                }
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
-            </div>
-            <div className='cart-container'>
-                
-                <Cart cart={cart}></Cart>
-            </div>
-        </div>
-    );
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json"
+    )
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+
+  useEffect(() => {
+    const storedCart = getShoppingCart();
+    const savedCart = [];
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+
+      if (addedProduct) {
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        savedCart.push(addedProduct);
+      }
+    }
+    setCart(savedCart);
+  }, [products]);
+
+  const handlerToClick = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+    addToDb(product.id);
+  };
+
+  return (
+    <div className="shop-container">
+      <div className="product-container">
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            handlerToClick={handlerToClick}
+          ></Product>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
